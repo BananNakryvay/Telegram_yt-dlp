7610c4bd1ff064ad9ae8c491b31961e18910ff20
EdemBey <100853918+EdemBey@users.noreply.github.com>
2024-09-02 02:23:57 +0200

    Change file name system + refactor  download_video()

--- bot.py
+++ bot.py
@@ -130,75 +130,46 @@ def stampcheck(msg, url, best_audio_id, timestamp, timestop, list_of_formats, vi
 
 def download_video(message, url, best_audio_id=None, start=None, end=None):
     resolution = message.text
+    is_audio_only = resolution == "MP3"
+    format_id = "bestaudio" if is_audio_only else resolution.split("ID:")[-1]
+    format_str = format_id if is_audio_only or not best_audio_id else f'{format_id}+{best_audio_id}'
+    extension = 'mp3' if is_audio_only else 'mkv'
+    output_path = os.path.join(DOWNLOAD_PATH, f'%(format_id)s{str(start or "")}{str(end or "")}%(id)s.{extension}')
 
-    if resolution == "MP3":
-        # Download only audio
-        format_id = "bestaudio"
-        output_filename = f"{random.randint(1, 10000)}.mp3"
-        output_path = os.path.join(DOWNLOAD_PATH, output_filename)
-        ydl_opts = {
-            'format': format_id,
-            'outtmpl': output_path,
-            'socket_timeout': 30,
-        }
+    ydl_opts = {
+        'format': format_str,
+        'outtmpl': output_path,
+        'socket_timeout': 30,
+    }
 
-        try:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                info_dict = ydl.extract_info(url, download=True)
-                video_file_path = parse_text(ip + f':{SERVER_PORT}/files/{output_filename}')
-                title = parse_text(info_dict['title'])
+    if start or end:
+        ydl_opts.update({
+            'verbose': True,
+            'download_ranges': download_range_func(None, [(float(start or 0), float(end or -1))]),
+            'force_keyframes_at_cuts': True
+        })
 
-                # Send the link for MP3
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=True)
+            output_filename = ydl.prepare_filename(info_dict).replace('\\', '/')
+            video_file_path = parse_text(f"{ip}:{SERVER_PORT}/files/{output_filename}")
+            title = parse_text(info_dict['title'])
+
+            file_size = os.path.getsize(output_filename)
+            if file_size < 50 * 1024 * 1024:  # 50 MB
+                with open(output_filename, 'rb') as media_file:
+                    if is_audio_only:
+                        bot.send_audio(message.chat.id, audio=media_file, title=title)
+                    else:
+                        bot.send_video(message.chat.id, video=media_file, caption=title,  supports_streaming=True)
+
                 bot.send_message(message.chat.id, f'[{title}]({video_file_path})', parse_mode='MarkdownV2')
 
-                # Schedule file deletion after 90 seconds
-                Timer(90, lambda: delete_file(output_path)).start()
-        except Exception as e:
-            bot.reply_to(message, f"Failed to download MP3: {e}")
-
-    else:
-        # Download video with specified format
-        format_id = resolution.split("ID:")[-1]
-        format_str = f'{format_id}+{best_audio_id}' if best_audio_id else format_id
-
-        # Generate a random 8-character ID
-        random_id = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
-
-        # Create the output filename
-        output_filename = f"{random_id}.mkv"
-        output_path = os.path.join(DOWNLOAD_PATH, output_filename)
-
-        ydl_opts = {
-            'format': format_str,
-            'outtmpl': output_path,
-            'socket_timeout': 30,
-            **({
-                'verbose': True,
-                'download_ranges': download_range_func(None, [(float(start or 0), float(end or -1))]),
-                'force_keyframes_at_cuts': True
-            } if start or end else {})
-        }
-
-        try:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                info_dict = ydl.extract_info(url, download=True)
-                video_file_path = parse_text(ip + f':{SERVER_PORT}/files/{output_filename}')
-                title = parse_text(info_dict['title'])
-
-                # Check the file size and send either video or link
-                file_size = os.path.getsize(output_path)
-                if file_size < 50 * 1024 * 1024: # 50 MB
-                    with open(output_path, 'rb') as video:
-                        # Send the video as a file
-                        bot.send_video(message.chat.id, video)
-                else:
-                    # Send the link if the file size is larger
-                    bot.send_message(message.chat.id, f'[{title}]({video_file_path})', parse_mode='MarkdownV2')
-
-                # Schedule file deletion after 90 seconds
-                Timer(90, lambda: delete_file(output_path)).start()
-        except Exception as e:
-            bot.reply_to(message, f"Failed to download video: {e}")
+            Timer(90, lambda: delete_file(output_filename)).start()
+    except Exception as e:
+        media_type = "MP3" if is_audio_only else "video"
+        bot.reply_to(message, f"Failed to download {media_type}: {e}")
 
 def parse_text(text):
     """Escape special characters in text for MarkdownV2."""
